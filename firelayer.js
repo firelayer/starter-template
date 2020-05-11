29614a2729a7dde0c12bc98d51c3785e93c45cc5
João Teixeira <joao@indielayer.com>
2020-05-11 11:44:23 +0100

    chore: change output colors

--- firelayer.js
+++ firelayer.js
@@ -9,8 +9,8 @@
 module.exports = async function({ chalk, open, logger, prompt, targetDir }) {
   const website = 'https://firelayer.io/templates/starter'
 
-  console.log(`Template Documentation - ${chalk.bold.cyan(website)}\n`)
-  console.log(`If you wish to add a landing page: ${chalk.bold.cyan('firelayer add:template landing')}`)
+  console.log(`\nTemplate Documentation - ${chalk.bold.green(website)}\n`)
+  console.log(`If you wish to add a landing page: ${chalk.bold.yellow('firelayer add:template landing')}`)
   console.log(`For other templates see - ${chalk.bold.cyan('https://firelayer.io/templates')}\n`)
 
   open(website)
